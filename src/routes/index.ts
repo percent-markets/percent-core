02fb3d8aba71dcadee72c8b233ed8e9d3933c7df
alexjaniak <alexander.janiak.acc@outlook.com>
2025-10-27 19:16:41 -0400

    Remove analytics page

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 import proposalRoutes from './proposals';
-import analyticsRoutes from './analytics';
 import twapRoutes from './twap';
 import vaultRoutes from './vaults';
 import swapRoutes from './swap';
@@ -16,7 +15,6 @@ router.get('/health', (_req, res) => {
 });
 
 router.use('/proposals', proposalRoutes);
-router.use('/analytics', analyticsRoutes);
 router.use('/twap', twapRoutes);
 router.use('/vaults', vaultRoutes);
 router.use('/swap', swapRoutes);
